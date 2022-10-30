50832464c01636367b7e581b75d7c0512d95c96e
Prasanth Rao <mechprasanthx@gmail.com>
2022-10-31 01:26:03 +0530

    Update TestCase01.java
    Last commit

--- src/test/java/test_package/TestCase01.java
+++ src/test/java/test_package/TestCase01.java
@@ -21,17 +21,15 @@ public class TestCase01 extends TestBase{
 		Loginpage Login=new Loginpage(driver);
 		Login.LoginTest("Admin","admin123");
 		
-		
-		
 	}
 	
 	@Test(priority=1)
 	public void Test02() {
+		
 		Loginpage Login=new Loginpage(driver);
 		Login.LoginTest("Admin","admin123");
 		
 		HomePages Home=new HomePages(driver);
-		
 		Home.click_on_usernamelabel();
 		Home.click_on_LogOut_Button();
 		
