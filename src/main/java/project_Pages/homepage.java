48d88f95a0582cf068422399daaf72d9ef1d7f6a
Prasanth Rao <mechprasanthx@gmail.com>
2022-10-30 22:49:10 +0530

    Second commmit from prasanth rao

--- src/main/java/project_Pages/homepage.java
+++ src/main/java/project_Pages/homepage.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class homepage {
 
@@ -19,13 +22,29 @@ public class homepage {
 	}
 		
 	@FindBy(name="username") WebElement Username;
+	@FindBy(name="password") WebElement Password;
+	@FindBy(xpath="//button[@type='submit']") WebElement Login_Button;
+	
+	
 	
 	public void Send_username(String username) {
 	
-  	   // driver.findElement(By.name("username")).sendKeys(username);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		Username.sendKeys(username);
+  	   WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+  	   wait.until(ExpectedConditions.visibilityOf(Username)).sendKeys(username);	
+	}
+	
+	public void Send_Password(String password) {
+		
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+	  	wait.until(ExpectedConditions.visibilityOf(Password)).sendKeys(password);
+	}
+	public void Click_On_LoginButton() {
+		
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+	  	wait.until(ExpectedConditions.elementToBeClickable(Login_Button)).click();
 		
 	}
+	
+	
 	
 }
--- src/test/java/test_package/TestCase01.java
+++ src/test/java/test_package/TestCase01.java
@@ -1,6 +1,7 @@
 package test_package;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import project_Pages.homepage;
@@ -16,8 +17,16 @@ public class TestCase01 extends TestBase{
 		driver.getTitle();
 		System.out.println(driver.getTitle());
 		homepage home=new homepage(driver);
+		
 		home.Send_username("Admin");
-		System.out.println("user value entered successfully");
+		Reporter.log("===User Entered Username===");
+		
+		home.Send_Password("admin123");
+		Reporter.log("===User Entered password===");
+		
+		home.Click_On_LoginButton();
+		Reporter.log("===User clicked login button===");
+		
 	}
 
 	
