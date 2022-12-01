d5658e49b9d41e89ab71fe4b06d9d5f5d2c3bcc5
Prasanth Rao <mechprasanthx@gmail.com>
2022-12-01 15:03:27 +0530

    latest commit from prasanth

---
+++ src/test/java/test_package/Sample01.java
@@ -0,0 +1,17 @@
+package test_package;
+
+public class Sample01 {
+   /*
+    * Class level variable can be made static.
+    * 
+    */
+	static int a=10;
+	
+	static void abc() {
+		
+		int b=12;
+	}
+	
+	
+	
+}
